b2cc93e569a155357110efc678c2b749a5b589a4
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-04-22 21:49:16 -0300

    feat(repository): create quarto searchByDate function

--- src/main/java/com/agendamento/hotel/repository/QuartoRepository.java
+++ src/main/java/com/agendamento/hotel/repository/QuartoRepository.java
@@ -2,9 +2,19 @@ package com.agendamento.hotel.repository;
 
 import com.agendamento.hotel.model.Quarto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface QuartoRepository extends JpaRepository <Quarto, Long> {
+
+    @Query("select q from Quarto q inner join Reserva r on q.id=r.quarto.id where r.data_entrada >= :data_entrada and r.data_saida <= :data_saida")
+    List<Quarto> searchByDate(
+            @Param("data_entrada") LocalDate data_entrada,
+            @Param("data_saida") LocalDate data_saida);
 
 }
