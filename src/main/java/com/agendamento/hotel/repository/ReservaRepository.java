6cc15ac07494d3e1cf754ea80ad398375f99b535
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-04-22 21:43:34 -0300

    feat(repository): create reserva searchByDate function

--- src/main/java/com/agendamento/hotel/repository/ReservaRepository.java
+++ src/main/java/com/agendamento/hotel/repository/ReservaRepository.java
@@ -4,9 +4,11 @@ import com.agendamento.hotel.model.Hospede;
 import com.agendamento.hotel.model.Quarto;
 import com.agendamento.hotel.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface ReservaRepository extends JpaRepository <Reserva, Long>{
 
@@ -14,4 +16,9 @@ public interface ReservaRepository extends JpaRepository <Reserva, Long>{
     List<Reserva> searchByHospede(Hospede hospede);
 
     List<Reserva> searchByQuarto(Quarto quarto);
+
+    @Query("select r from Reserva r where r.data_entrada >= :data_entrada and r.data_saida <= :data_saida")
+    List<Reserva> searchByDate(
+        @Param("data_entrada") LocalDate data_entrada,
+        @Param("data_saida") LocalDate data_saida);
 }
