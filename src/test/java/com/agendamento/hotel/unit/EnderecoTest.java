0eb31317288a084b24aa8d08fea20ebb086ec173
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-07-01 22:50:41 -0300

    feat(tests): add endereco fakers

--- src/test/java/com/agendamento/hotel/unit/EnderecoTest.java
+++ src/test/java/com/agendamento/hotel/unit/EnderecoTest.java
@@ -2,28 +2,34 @@ package com.agendamento.hotel.unit;
 
 import com.agendamento.hotel.model.Endereco;
 import com.agendamento.hotel.service.EnderecoService;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class EnderecoTest {
 
     @Autowired
     private EnderecoService enderecoService;
+    private Faker faker = new Faker(new Locale("pt-br"));
 
     @Test
     void store() {
 
         Endereco endereco = new Endereco();
-        endereco.setEstado("Paraná");
-        endereco.setCidade("Foz do Iguaçu");
-        endereco.setLogradouro("Rua Fagundes Varela");
-        endereco.setNumero("664");
-        endereco.setComplemento("apto 01");
+        endereco.setEstado(faker.address().state());
+        endereco.setCidade(faker.address().city());
+        endereco.setLogradouro(faker.address().streetName());
+        endereco.setNumero(faker.address().buildingNumber());
+        endereco.setComplemento(faker.address().secondaryAddress());
 
         Endereco resutdb = enderecoService.store(endereco);
         Assertions.assertEquals(endereco, resutdb);
@@ -32,11 +38,11 @@ public class EnderecoTest {
     @Test
     void show() {
         Endereco endereco = new Endereco();
-        endereco.setEstado("Paraná");
-        endereco.setCidade("Foz do Iguaçu");
-        endereco.setLogradouro("Rua Fagundes Varela");
-        endereco.setNumero("664");
-        endereco.setComplemento("apto 01");
+        endereco.setEstado(faker.address().state());
+        endereco.setCidade(faker.address().city());
+        endereco.setLogradouro(faker.address().streetName());
+        endereco.setNumero(faker.address().buildingNumber());
+        endereco.setComplemento(faker.address().secondaryAddress());
 
         enderecoService.store(endereco);
 
@@ -45,41 +51,41 @@ public class EnderecoTest {
     }
 
     @Test
-    void index(){
+    void index() {
         List<Endereco> before = enderecoService.index();
-        
+
         Endereco endereco = new Endereco();
-        endereco.setEstado("Paraná");
-        endereco.setCidade("Foz do Iguaçu");
-        endereco.setLogradouro("Rua Fagundes Varela");
-        endereco.setNumero("664");
-        endereco.setComplemento("apto 01");
+        endereco.setEstado(faker.address().state());
+        endereco.setCidade(faker.address().city());
+        endereco.setLogradouro(faker.address().streetName());
+        endereco.setNumero(faker.address().buildingNumber());
+        endereco.setComplemento(faker.address().secondaryAddress());
 
         Endereco endereco2 = new Endereco();
-        endereco.setEstado("Paraná");
-        endereco.setCidade("Foz do Iguaçu");
-        endereco.setLogradouro("Rua das Missões");
-        endereco.setNumero("554");
-        endereco.setComplemento("apto 01");
+        endereco.setEstado(faker.address().state());
+        endereco.setCidade(faker.address().city());
+        endereco.setLogradouro(faker.address().streetName());
+        endereco.setNumero(faker.address().buildingNumber());
+        endereco.setComplemento(faker.address().secondaryAddress());
 
-        enderecoService. store(endereco);
+        enderecoService.store(endereco);
         enderecoService.store(endereco2);
 
         List<Endereco> after = enderecoService.index();
-        Assertions.assertEquals(after.size(), before.size()+2);
+        Assertions.assertEquals(after.size(), before.size() + 2);
     }
 
     @Test
     void update() {
         Endereco endereco = new Endereco();
-        endereco.setEstado("Paraná");
-        endereco.setCidade("Foz do Iguaçu");
-        endereco.setLogradouro("Rua Fagundes Varela");
-        endereco.setNumero("664");
-        endereco.setComplemento("apto 01");
+        endereco.setEstado(faker.address().state());
+        endereco.setCidade(faker.address().city());
+        endereco.setLogradouro(faker.address().streetName());
+        endereco.setNumero(faker.address().buildingNumber());
+        endereco.setComplemento(faker.address().secondaryAddress());
 
         Endereco resultdb = enderecoService.store(endereco);
-        resultdb.setComplemento("ap 02");
+        resultdb.setComplemento(faker.address().secondaryAddress());
 
         Endereco updateEndereco = enderecoService.update(resultdb);
         Assertions.assertEquals(resultdb.getComplemento(), updateEndereco.getComplemento());
@@ -88,13 +94,13 @@ public class EnderecoTest {
     @Test
     void destroy() {
         List<Endereco> before = enderecoService.index();
-        
+
         Endereco endereco = new Endereco();
-        endereco.setEstado("Paraná");
-        endereco.setCidade("Foz do Iguaçu");
-        endereco.setLogradouro("Rua Fagundes Varela");
-        endereco.setNumero("664");
-        endereco.setComplemento("apto 01");
+        endereco.setEstado(faker.address().state());
+        endereco.setCidade(faker.address().city());
+        endereco.setLogradouro(faker.address().streetName());
+        endereco.setNumero(faker.address().buildingNumber());
+        endereco.setComplemento(faker.address().secondaryAddress());
 
         Endereco resultdb = enderecoService.store(endereco);
 
