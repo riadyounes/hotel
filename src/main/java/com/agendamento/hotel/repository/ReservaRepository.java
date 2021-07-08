40849545cc327d427f3accbc56635f2c287dfcc9
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-07-08 00:41:10 -0300

    Feat/vic improve reserva search (#37)
    * style(enum): rename estado enum
    
    * refactor(service): update estado enum import
    
    * refactor(test): update estado enum import
    
    * refactor(service): reserva search
    
    * refactor(repository): reserva search
    
    * refactor(controller): reserva search

--- src/main/java/com/agendamento/hotel/repository/ReservaRepository.java
+++ src/main/java/com/agendamento/hotel/repository/ReservaRepository.java
@@ -1,7 +1,6 @@
 package com.agendamento.hotel.repository;
 
-import com.agendamento.hotel.model.Hospede;
-import com.agendamento.hotel.model.Quarto;
+import com.agendamento.hotel.enums.ReservaEstado;
 import com.agendamento.hotel.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,15 +9,17 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface ReservaRepository extends JpaRepository <Reserva, Long>{
-
-
-    List<Reserva> searchByHospede(Hospede hospede);
-
-    List<Reserva> searchByQuarto(Quarto quarto);
-
-    @Query("select r from Reserva r where r.data_entrada >= :data_entrada and r.data_saida <= :data_saida")
-    List<Reserva> searchByDate(
-        @Param("data_entrada") LocalDate data_entrada,
-        @Param("data_saida") LocalDate data_saida);
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    @Query("select r from Reserva r " +
+            "where r.estado= :estado " +
+            "or r.quarto.id= :quarto_id " +
+            "or r.hospede.id= :hospede_id " +
+            "or r.data_entrada >= :data_entrada " +
+            "or r.data_saida <= :data_saida")
+    List<Reserva> search(
+            @Param("data_entrada") LocalDate data_entrada,
+            @Param("data_saida") LocalDate data_saida,
+            @Param("estado") ReservaEstado estado,
+            @Param("hospede_id") Long hospede_id,
+            @Param("quarto_id") Long quarto_id);
 }
